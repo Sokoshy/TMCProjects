8b05c06911e31bd64540468b70373263a52ef7ac
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2025-01-25 14:27:12 +0100

    complete exercice section 4 of part 2

--- Java-Programming-1/Part-2/part02-Part02_31.Greatest/part02/Part02_31.Greatest/src/main/java/Greatest.java
+++ Java-Programming-1/Part-2/part02-Part02_31.Greatest/part02/Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,15 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
-
-        return -1;
+        int greatest;
+        if (number1 > number2 && number1 > number3) {
+            greatest = number1;
+        } else if (number2 > number3 && number2 > number1) {
+            greatest = number2;
+        }else{
+            greatest = number3;
+        }
+        return greatest;
     }
 
     public static void main(String[] args) {
